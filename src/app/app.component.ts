243b409dc06a52c3a7ed3654125b8dfc92252309
Yazur Garg <ygarg1@myseneca.ca>
2022-04-15 01:37:34 -0400

    link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 /*********************************************************************************
-* BTI425 – Assignment 03
+* BTI425 – Assignment 04
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students.
 *
 * Name: Yazur Garg Student ID: 133395202 Date: 2022/03/22
-*
+*https://bti425assignment4.herokuapp.com
 ********************************************************************************/
 
 import { Component } from '@angular/core';
